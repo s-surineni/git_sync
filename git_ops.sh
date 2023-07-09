6a61f86d7aacc9b4ccc0cd8e36cf1b565723ee0a
sampath <sampath.surineni@gmail.com>
2023-07-09 13:52:06 +0530

    update

--- git_ops.sh
+++ git_ops.sh
@@ -3,5 +3,5 @@ repo_locs=(/home/sampath/projects/my_notes
            /home/sampath/projects/git_sync)
 for repo in "${repo_locs[@]}"; do
     echo `pwd`
-    (cd "${repo}" && git pull && git add * && git commit -m "update" && git push)
+    (cd "${repo}" && git pull &&  git commit -am "update" && git push)
 done
