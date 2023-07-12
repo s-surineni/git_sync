c70671debf63d8f12319559381c19107c38b22f1
sampath surineni <ssurineni@salesforce.com>
2023-07-12 21:04:00 +0530

    used time and hostname

--- git_ops.sh
+++ git_ops.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 date
-repo_locs=(/home/sampath/projects/my_notes
-           /home/sampath/projects/git_sync
-           /home/sampath/projects/settings)
+repo_locs=(/Users/ssurineni/ironman/my_notes
+            /Users/ssurineni/salesforce/my_notes
+            /Users/ssurineni/ironman/projects/atice)
 for repo in "${repo_locs[@]}"; do
-    (cd "${repo}" && echo `pwd` && git commit -am "update" && git pull )
+    (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(hostname)" && git pull )
     cd "${repo}" && git push
 done
