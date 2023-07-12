a773accb96972e06ce30d1838083d941d385c4e8
sampath <sampath.surineni@gmail.com>
2023-07-12 20:20:04 +0530

    update

--- git_ops.sh
+++ git_ops.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-repo_locs=(/Users/ssurineni/ironman/my_notes
-            /Users/ssurineni/salesforce/my_notes)
+date
+repo_locs=(/home/sampath/projects/my_notes
+           /home/sampath/projects/git_sync
+           /home/sampath/projects/settings)
 for repo in "${repo_locs[@]}"; do
     (cd "${repo}" && echo `pwd` && git commit -am "update" && git pull )
     cd "${repo}" && git push
