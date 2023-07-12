1b418b4774003aac629dc8ffa888bac2514ba486
sampath surineni <ssurineni@salesforce.com>
2023-07-12 21:08:05 +0530

    replaced system name with username

--- git_ops.sh
+++ git_ops.sh
@@ -4,6 +4,6 @@ repo_locs=(/Users/ssurineni/ironman/my_notes
             /Users/ssurineni/salesforce/my_notes
             /Users/ssurineni/ironman/projects/atice)
 for repo in "${repo_locs[@]}"; do
-    (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(hostname)" && git pull )
+    (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)" && git pull )
     cd "${repo}" && git push
 done
