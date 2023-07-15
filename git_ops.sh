3d56811dcdcb42c9fea100566fbf435c619e6e0c
sampath surineni <ssurineni@salesforce.com>
2023-07-15 17:32:11 +0530

    changed pull to new line

--- git_ops.sh
+++ git_ops.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "time $(date)"
-repo_locs=(/home/sampath/projects/my_notes
-           /home/sampath/projects/git_sync
-           /home/sampath/projects/settings)
+repo_locs=(/Users/ssurineni/ironman/my_notes
+            /Users/ssurineni/salesforce/my_notes
+            /Users/ssurineni/ironman/projects/atice)
 for repo in "${repo_locs[@]}"; do
     echo "******************************************************************"
     if [ "$repo" = "/Users/ssurineni/ironman/projects/atice" ]; then
     cd "${repo}" && git add *.py
     fi
-    (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)" && git pull )
+    (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)")
+    cd "${repo}" && git pull
     cd "${repo}" && git push
 done
