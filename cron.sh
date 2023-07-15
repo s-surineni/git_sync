acda507eb5d8ebecb98a03313137029e4bc16ea2
sampath surineni <ssurineni@salesforce.com>
2023-07-15 11:45:41 +0530

    new changes

--- cron.sh
+++ cron.sh
@@ -1,2 +1,2 @@
 */10 * * * * /path/to/update.sh
-*/10 * * * * /Users/ssurineni/projects/git_sync/git_ops.sh  >> /Users/ssurineni/logs/git-sync.log 2>&1
+*/10 * * * * /Users/ssurineni/projects/git_sync/git_ops.sh  > /Users/ssurineni/logs/git-sync.log 2>&1
--- git_ops.sh
+++ git_ops.sh
@@ -4,6 +4,10 @@ repo_locs=(/Users/ssurineni/ironman/my_notes
             /Users/ssurineni/salesforce/my_notes
             /Users/ssurineni/ironman/projects/atice)
 for repo in "${repo_locs[@]}"; do
+    echo "******************************************************************"
+    if [ "$repo" = "/Users/ssurineni/ironman/projects/atice" ]; then
+    cd "${repo}" && git add *.py
+    fi
     (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)" && git pull )
     cd "${repo}" && git push
 done
