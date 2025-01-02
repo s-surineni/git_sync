73a7b546c56d51097712fa7d133448f4902839a5
ssurineni <ssurineni@salesforce.com>
2025-01-02 09:47:25 +0530

    written a function

--- git_ops.sh
+++ git_ops.sh
@@ -3,11 +3,18 @@ echo "time $(date)"
 repo_locs=(/Users/ssurineni/ironman/my_notes
             # /Users/ssurineni/salesforce/my_notes
             /Users/ssurineni/ironman/atice
+            /Users/ssurineni/ironman/problem-solving
           )
+
+function commit_and_push {
+    cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)"
+    cd "${repo}" && git pull
+    cd "${repo}" && git push
+}
 for repo in "${repo_locs[@]}"; do
     echo "******************************************************************"
     if [[ $repo == *"atice"* ]]; then
-        cd "${repo}" && git add *.py
+        cd "${repo}" && git add *.py && git add *.js
     fi
     # if [ "$repo" = "/Users/ssurineni/ironman/projects/atice" ]; then
     # cd "${repo}" && git add *.py
@@ -15,7 +22,11 @@ for repo in "${repo_locs[@]}"; do
     if [ "$repo" = "/Users/ssurineni/ironman/my_notes" ]; then
         cd "${repo}" && git add *.org
     fi
-    (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)")
-    cd "${repo}" && git pull
-    cd "${repo}" && git push
+    if [ "$repo" = "/Users/ssurineni/ironman/problem-solving" ]; then
+        cd "${repo}" && git add *.org
+    fi
+    # (cd "${repo}" && echo `pwd` && git commit -am "$(date) $(whoami)")
+    # cd "${repo}" && git pull
+    # cd "${repo}" && git push
+    commit_and_push ${repo}
 done
